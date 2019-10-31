85f11c2b78782f905bad74df9ec03de7bf073979
Võ Minh Triều <minhtrieuvo600@gmail.com>
2019-10-31 21:45:15 +0700

    Deleted old files

--- Find_EPIs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <unordered_set>
-#include "Find_Prime_Implicants.h"
-
-void find_EPIs(vector<Implicant> &prime_implicants, vector<vector<Implicant*>> &final_minimum_function);
--- MatrixAndVector/function.h
+++
@@ -1,5 +0,0 @@
-#include "Matrix.h"
-
-Vector CreateVectorWithLast(int length, double last);
-void PrintRoot(Matrix x);
-void Solve(Matrix A, Matrix b);
